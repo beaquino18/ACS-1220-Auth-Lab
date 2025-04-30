66b3d9535fc0f0ce07adcf992673aa06595a7540
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2025-04-29 23:31:23 -0700

    used partial for form fields login and sign up

--- books_app/auth/routes.py
+++ books_app/auth/routes.py
@@ -11,14 +11,40 @@ auth = Blueprint("auth", __name__)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    # TODO: Fill out this route!
-    pass
-
+    # initialize a SignUpForm instance from forms.py
+    form = SignUpForm()
+    
+    # If the form is valid,
+    if form.validate_on_submit():
+        # Generate a hashed passowrd using bcrypt library
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        # Create a new User with given username and hashed password
+        user = User(
+            username = form.username.data,
+            password = hashed_password
+        )
+        
+        # Commit to database
+        db.session.add(user)
+        db.session.commit()
+        
+        # Flash a success message to the user
+        flash('Account created successfully')
+        # Redirect to login page
+        return redirect(url_for('auth.login'))
+    return render_template('signup.html', form=form)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    # TODO: Fill out this route!
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        # This would allow user to go back where they were before
+        next_page = request.args.get('next')
+        
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
 
 @auth.route('/logout')
 def logout():
